23989af3f2ed1af9568e8df6412d2a5d3344f48f
Rain <wxynihao@gmail.com>
2018-05-13 14:04:14 +0800

    添加jedis的redis命令测试

--- src/test/java/me/rainking/redisdemo/jedis/JedisDemo.java
+++ src/test/java/me/rainking/redisdemo/jedis/JedisDemo.java
@@ -5,6 +5,10 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 /**
  * @author Rain
  * @description
@@ -18,12 +22,12 @@ public class JedisDemo {
     @Test
     public void basicUse() {
         Jedis jedis = new Jedis("127.0.0.1", 6379);
+        System.out.println("服务正在运行: " + jedis.ping());
         jedis.set("name", "rain");
         String val = jedis.get("name");
         System.out.println(val);
         jedis.close();
     }
-
 
     /**
      * 使用连接池
@@ -42,4 +46,40 @@ public class JedisDemo {
         jedis.close();
         jedisPool.close();
     }
+
+    /**
+     * 使用数据结构List
+     */
+    @Test
+    public void RedisList() {
+        //连接本地的 Redis 服务
+        Jedis jedis = new Jedis("localhost");
+        //存储数据到列表中
+        jedis.lpush("site-list", "Runoob");
+        jedis.lpush("site-list", "Google");
+        jedis.lpush("site-list", "Taobao");
+        // 获取存储的数据并输出
+        List<String> list = jedis.lrange("site-list", 0, 2);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+    }
+
+    /**
+     * 使用命令Keys
+     */
+    @Test
+    public void RedisKey() {
+        //连接本地的 Redis 服务
+        Jedis jedis = new Jedis("localhost");
+        // 获取数据并输出
+        Set<String> keys = jedis.keys("*");
+        Iterator<String> it=keys.iterator() ;
+        while(it.hasNext()){
+            String key = it.next();
+            System.out.println(key);
+        }
+    }
+
+
 }
